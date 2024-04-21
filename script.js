d8baf4ef79c05fe8fffb6f4eabffd6da4aa78bd1
jlvirg <159857413+jlvirg@users.noreply.github.com>
2024-04-21 12:58:17 +1000

    feat: Added button and JS functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+function gotoRecipe(){
+    var radios = document.getElementsByName('recipe'), 
+        value  = '';
+
+    for (var i = radios.length; i--;) {
+        if (radios[i].checked) {
+            value = radios[i].value;
+            break;
+        }
+    }
+
+             window.location.href='recipes/'+ value + '.html'
+        }
